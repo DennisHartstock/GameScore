6f93452c759fad588493fa1d548c66d8f14c287c
DennsHartstock <dennis.hartstock@web.de>
2022-11-18 07:57:21 +0100

    override onSaveInstanceState and onDestroy methods

--- app/src/main/java/com/example/gamescore/MainActivity.java
+++ app/src/main/java/com/example/gamescore/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gamescore;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            score1 = savedInstanceState.getInt("score1");
+            score2 = savedInstanceState.getInt("score2");
+        }
+
         TextView tvScore1 = findViewById(R.id.tvScore1);
         TextView tvScore2 = findViewById(R.id.tvScore2);
 
@@ -22,5 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
         tvScore1.setOnClickListener(view -> tvScore1.setText(String.valueOf(++score1)));
         tvScore2.setOnClickListener(view -> tvScore2.setText(String.valueOf(++score2)));
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("score1", score1);
+        outState.putInt("score2", score2);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
